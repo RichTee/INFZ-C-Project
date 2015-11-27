31dead32debff1e26093d368109e49dce6819511
MrRichTee <ostmyst@gmail.com>
2015-11-27 20:04:58 +0100

    [Core/Misc] HTML, CSS, Controller: Added CSHTML and CSS along with the necessary HomeController. - Added Index.cshtml - Added CSS - Added HomeController - Added Mysql.Data.dll (Currently unused)
    Did not test on seperate machine.
    
    MAKE SURE TO READ THE INCLUDED README.TXT

---
+++ Applicatie/CBlokHerkansing/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBlokHerkansing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Brownie candy canes halvah marshmallow halvah chocolate. Bonbon oat cake cake fruitcake chocolate cake. Lemon drops candy canes chocolate cake gummies tiramisu cheesecake jelly-o jujubes sweet. Icing toffee brownie gummi bears halvah.";
+
+            return View();
+        }
+    }
+}
