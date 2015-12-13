de7c0a84186fc5e5687839e7df91f24b79abaaf8
MrRichTee <ostmyst@gmail.com>
2015-12-13 19:21:10 +0100

    [Core/Misc] Started new project.
    - Core will be the primary work environment, changes to the website should be done here.
    - Template is to be remain unchanged and only be used as reference.
    - Database Base will consist of only the Database SQL
    - Database Dump will consist of only the Database data dump based on the Database Base.
    
    TODO:
    - Add side bar (Shopping Cart)
    - Add DB connection
    - Add login
    - Add CRUD product

---
+++ Applicatie/Core/CBlokHerkansing/Controllers/Account/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBlokHerkansing.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login", "AccountController"); // Ternary Operator when already logged in to redirect to somewhere else.
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index", "HomeController");
+        }
+    }
+}
---
+++ Applicatie/Core/CBlokHerkansing/Models/Account/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CBlokHerkansing.Models.Account
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Gebruikersnaam is vereist")] // Dit MOET aanwezig zijn
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Wachtwoord is vereist")]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+    }
+}
