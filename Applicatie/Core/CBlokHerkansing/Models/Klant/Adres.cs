a5971aa954f766790078a5e9bc83a58791360f44
MrRichTee <ostmyst@gmail.com>
2015-12-26 21:20:48 +0100

    Added Adres model.

---
+++ Applicatie/Core/CBlokHerkansing/Models/Klant/Adres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CBlokHerkansing.Models.Klant
+{
+    public class Adres
+    {
+        public int id { get; set; }
+        public string straat { get; set; }
+        public string postcode { get; set; }
+        public string huisnummer { get; set; }
+        public string huisnummertoevoeging { get; set; }
+        public string stad { get; set; }
+        public int gebruikerId { get; set; }
+
+    }
+}
