cd619d38b1728ec07d43e08f18f8f5ba6055b739
MrRichTee <ostmyst@gmail.com>
2016-01-17 23:35:14 +0100

    Core/Misc: Added error code handling.

---
+++ Applicatie/Core/CBlokHerkansing/ViewModels/Product/ToevoegenProductDetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CBlokHerkansing.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBlokHerkansing.ViewModels.Product
+{
+    public class ToevoegenProductDetailViewModel
+    {
+        public List<ProductBase> productBaseList { get; set; }
+
+        public ProductDetail productDetail { get; set; }
+
+        public SelectList listStatus { get; set; }
+
+        [Required(ErrorMessage = "U moet een bestel status selecteren")]
+        public string SelectedStatus { get; set; }
+    }
+}
